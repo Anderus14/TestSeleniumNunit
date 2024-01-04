9913a66cc0c7eb0e5c9a63aedb51930105a8d459
Mateusz <mateusz.malolepszy@gmail.com>
2024-01-04 17:48:16 +0100

    Added assert default button text before entering any value into an input

--- TestProjectNunit/UnitTest1.cs
+++ TestProjectNunit/UnitTest1.cs
@@ -8,37 +8,38 @@ using WebDriverManager.Helpers;
 
 namespace TestProjectNunit
 {
-		public class Tests
-		{
+	public class Tests
+	{
 		private TextInputPage textInputPage;
 
-				private IWebDriver webDriver;
+		private IWebDriver webDriver;
 
-				[SetUp]
-				public void Setup()
-				{         
-					var options = new ChromeOptions();
-					//options.AddArguments("headless"); 
-					new DriverManager().SetUpDriver(new ChromeConfig(), "Latest", Architecture.X64);
-					webDriver = new ChromeDriver(options);
-					webDriver.Navigate().GoToUrl("http://uitestingplayground.com/textinput");
-					webDriver.Manage().Window.Maximize();
-					textInputPage = new TextInputPage(webDriver);
-				}
-
-				[TearDown]
-				public void Teardown()
-				{
-						webDriver.Close();
-						webDriver.Quit();
-				}
-
-				[Test]
-				public void FirstTest()
-				{
-						textInputPage.EnterTextIntoInput("dupa");
-						textInputPage.ClickButton();
-						Assert.That(textInputPage.Button.Text == "dupa");
-				}
+		[SetUp]
+		public void Setup()
+		{
+			var options = new ChromeOptions();
+			//options.AddArguments("headless"); 
+			new DriverManager().SetUpDriver(new ChromeConfig(), "Latest", Architecture.X64);
+			webDriver = new ChromeDriver(options);
+			webDriver.Navigate().GoToUrl("http://uitestingplayground.com/textinput");
+			webDriver.Manage().Window.Maximize();
+			textInputPage = new TextInputPage(webDriver);
 		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			webDriver.Close();
+			webDriver.Quit();
+		}
+
+		[Test]
+		public void FirstTest()
+		{
+			Assert.That(textInputPage.Button.Text.Equals("Button That Should Change it's Name Based on Input Value"));
+			textInputPage.EnterTextIntoInput("dupa");
+			textInputPage.ClickButton();
+			Assert.That(textInputPage.Button.Text.Equals("dupa"));
+		}
+	}
 }
